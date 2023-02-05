495304a948bd073d0bc31db8296450e0cf4b25f0
white <exwhite@naver.com>
2023-02-05 16:02:58 +0900

    Update mini blog

---
+++ mini-blog/src/component/page/MainPage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import PostList from "../list/PostList";
+import Button from "../ui/Button";
+import data from "../../data.json";
+
+// Button 컴포넌트를 이용해서 글 작성하기 페이지로 이동할 수 있게 함
+// PostList를 이용해서 글 목록을 표시 함
+// 페이지 이동을 위해 react-router-dom 의 useNavigate 훅을 사용
+
+const Wrapper = styled.div`
+  padding: 16px;
+  width: calc(100% - 32px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    <max-width:720px></max-width:720px>;
+
+    & >* {
+        "+:not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+// MainPage는 지금까지 만들어 놓은 컴포넌트들을 모아 놓은 용도
+function MainPage(props) {
+  const {} = props;
+
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <Container>
+        <Button
+          title="글 작성하기"
+          onClick={() => {
+            navigate("/post-write");
+          }}
+        />
+
+        <PostList
+          posts={data}
+          onClickItem={(item) => {
+            navigate(`/post/${item.id}`);
+          }}
+        />
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default MainPage;
---
+++ mini-blog/src/component/page/PostWritePage.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
+
+const Wrapper = styled.div`
+    padding: 16px
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
+
+  & > * {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
+
+// 제목을 위한, 글의 내용을 위한 state 2개를 가지고 있다
+// 실제 화면에 나타나는 부분은 TextInput을 사용해서 글의 제목과 내용을 각각 입력 받을 수 있도록 구성
+// 화면 최하단에는 Button 컴포넌트를 사용해서 글 작성 버튼은 추가
+function PostWritePage(props) {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <Wrapper>
+      <Container>
+        <TextInput
+          height={20}
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <TextInput
+          height={400}
+          value={content}
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+        />
+
+        <Button
+          title="글 작성하기"
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default PostWritePage;
